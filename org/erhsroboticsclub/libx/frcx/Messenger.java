e135bb9becf4a1dd54949d7a6c016e9d90434424
Michael Stevens <mls0.5mv2@gmail.com>
2013-10-29 19:20:17 -0400

    changing messenger class to now use ints for line numbers

--- org/erhsroboticsclub/libx/frcx/Messenger.java
+++ org/erhsroboticsclub/libx/frcx/Messenger.java
@@ -27,9 +27,7 @@ public class Messenger {
     public Messenger() {
         driverLCD = DriverStationLCD.getInstance();
         msg = new String[LINE.length];
-        for (int i = 0; i < msg.length; i++) {
-            msg[i] = " ";
-        }
+        for (int i = 0; i < msg.length; i++) msg[i] = " ";
     }
 
     /**
@@ -72,11 +70,13 @@ public class Messenger {
      * Prints a message to a specific line on the Driver Station LCD
      *
      * @param s The String to be printed on the Driver Station
-     * @param line The line to print the message to
+     * @param line The line to print the message to as an int from [1, 6]
      */
-    public void printOnLn(String s, DriverStationLCD.Line line) {
-        driverLCD.println(line, 1, EMPTY_SPACE);
-        driverLCD.println(line, 1, s);
+    public void printOnLn(String s, int i) {
+		i = Math.max(i - 1, 0);
+		i = Math.min(i, 5);
+        driverLCD.println(LINE[i], 1, EMPTY_SPACE);
+        driverLCD.println(LINE[i], 1, s);
         driverLCD.updateLCD();
     }
 
